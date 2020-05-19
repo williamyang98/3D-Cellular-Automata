a99b4c5e6adac5946c55522bf9590fe2b4614b42
FiendChain <formulating.response@gmail.com>
2020-05-20 08:35:30 +1000

    Added rules from video

--- src/app/RuleBrowser.js
+++ src/app/RuleBrowser.js
@@ -6,22 +6,47 @@ export class RuleBrowser {
     this.entries = [];
     this.add_entry(
       new RuleEntry(
-        '445', '4|4|5', 
+        '445', '4/4/5/M', 
         new Rule3D(n => n === 4, n => n === 4, 5),
         new SeedCrystal(0.2, 0.2)
       ));
     this.add_entry(
       new RuleEntry(
-        '678 678', '6-8|6-8|3',
+        '678 678', '6-8/6-8/3/M',
         new Rule3D(n => range(n,6,8), n => range(n,6,8), 3),
         new SeedCrystal(0.3)
       ));
     this.add_entry(
       new RuleEntry(
-        'Amoeba', '9-26|5-7,12-13,15|5',
+        'Amoeba', '9-26/5-7,12-13,15/5/M',
         new Rule3D(n => range(n,9,26), n => range(n,5,7) || range(n,12,13) || (n === 15), 5),
         new SeedCrystal(0.3),
       ));
+
+    this.add_entry(
+      new RuleEntry(
+        'Builder', '2,6,9/4,6,8-9/10/M',
+        new Rule3D(n => n === 2 || n === 6 || n === 9, n => n === 4 || n === 8 || range(n,8,9), 10),
+        new SeedCrystal(0.3, 0.15)
+      )
+    );
+
+    this.add_entry(
+      new RuleEntry(
+        'Clouds 1', '13-26/13-14,17-19/2/M',
+        new Rule3D(n => range(n,13,26), n => range(n,13,14) || range(17,19), 2),
+        new SeedCrystal(0.75, 0.5)
+      )
+    );
+
+    this.add_entry(
+      new RuleEntry(
+        'Spiky Growth', 'Long',
+        new Rule3D(n => range(n,0,3) || range(n,7,9) || range(n,11,13) || n == 18 || range(n,21,22) || range(n,24,26),
+                   n => n === 4 || n === 13 || n === 17 || range(n,20,24) || n === 26, 4),
+        new SeedCrystal(0.4, 0.05)
+      )
+    );
 
     this.selected_entry = 0;
   }
