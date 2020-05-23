2d3e59a985afdccb5a24ae8bbde59ecb0f4530ad
FiendChain <formulating.response@gmail.com>
2020-05-23 22:43:35 +1000

    Fixed controls

--- src/ui/Controls.jsx
+++ src/ui/Controls.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 export function Controls () {
-  const is_running = useSelector(state => state.sim.is_running);
+  const is_running = useSelector(state => state.sim.running);
   const dispatch = useDispatch();
 
   const on_off = is_running ? 'Pause': 'Run';
@@ -11,7 +11,7 @@ export function Controls () {
 
   return (
     <div className="btn-group">
-      <button className="btn btn-secondary" onClick={() => dispatch({type: 'tick'})}>Tick</button>
+      <button className="btn btn-secondary" onClick={() => dispatch({type: 'step'})}>Tick</button>
       <button className="btn btn-primary" onClick={() => dispatch({type: 'randomise'})}>Randomise</button>
       <button className="btn btn-warning" onClick={() => dispatch({type: 'clear'})}>Clear</button>
       <button className={"btn btn-"+run_btn} onClick={() => dispatch({type: 'toggle'})}>{on_off}</button>
