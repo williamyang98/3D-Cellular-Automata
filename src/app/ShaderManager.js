5dd61a305eda5cb0048b164a22aeb3abccd58c11
FiendChain <formulating.response@gmail.com>
2020-05-24 00:33:08 +1000

    Refactored shader into manager

---
+++ src/app/ShaderManager.js
@@ -0,0 +1,73 @@
+import { Shader } from '../gl/Shader';
+import { UniformMat4f, UniformVec3f, Uniform } from '../gl/Uniform';
+
+import { fragment_shader_src } from '../shaders/fragment_shader';
+import { vertex_shader_src } from '../shaders/vertex_shader';
+import { vec3 } from 'gl-matrix';
+
+export class ShaderManager {
+  constructor(gl, camera) {
+    this.gl = gl;
+    this.size = vec3.create();
+    this.camera = camera;
+
+    this.create();
+  }
+
+  set_size(size) {
+    for (let i = 0; i < 3; i++)
+      this.size[i] = size[i];
+  }
+
+  create() {
+    let gl = this.gl;
+    this.shaders = [];
+    for (let name in vertex_shader_src) {
+      let vert_src = vertex_shader_src[name];
+      let frag_src = fragment_shader_src;
+      let shader = new Shader(gl, vert_src, frag_src); 
+      this.add_uniforms(shader);
+
+      this.shaders.push({name:name, shader:shader});
+    }
+    this.current_shader = 0;
+  }
+
+  select_shader(index) {
+    this.current_shader = index;
+  }
+
+  bind() {
+    let shader_entry = this.shaders[this.current_shader];
+    let shader = shader_entry.shader;
+    shader.bind();
+  }
+
+  add_uniforms(shader) {
+    let gl = this.gl;
+
+    shader.add_uniform("uModel", new UniformMat4f(gl, this.camera.model));
+    shader.add_uniform("uView", new UniformMat4f(gl, this.camera.view));
+    shader.add_uniform("uProjection", new UniformMat4f(gl, this.camera.projection));
+    shader.add_uniform("uGridSize", new UniformVec3f(gl, this.size));
+    shader.add_uniform("uViewPosition", new UniformVec3f(gl, this.camera.view_position));
+
+    // lighting
+    let light_position = vec3.create();
+    vec3.scale(light_position, this.size, 2.5);
+    shader.add_uniform('light.position', new UniformVec3f(gl, light_position));
+    shader.add_uniform('light.colour', new UniformVec3f(gl, vec3.fromValues(1,1,1)));
+    // // lighting params
+    shader.add_uniform("uAmbientStrength", new Uniform(loc => gl.uniform1f(loc, 0.3)));
+    shader.add_uniform("uDiffuseStrength", new Uniform(loc => gl.uniform1f(loc, 0.9)));
+    shader.add_uniform("uSpecularStrength", new Uniform(loc => gl.uniform1f(loc, 0.5)));
+    shader.add_uniform("uSpecularPowerFactor", new Uniform(loc => gl.uniform1f(loc, 4.0)));
+    // add texture id
+    shader.add_uniform("uStateTexture", new Uniform(loc => gl.uniform1i(loc, 0)));
+    shader.add_uniform("uStateColourTexture", new Uniform(loc => gl.uniform1i(loc, 1)));
+    // post processing
+    shader.add_uniform("uScalingEnabled", new Uniform(loc => gl.uniform1i(loc, 0)));
+    shader.add_uniform("uFogNear", new Uniform(loc => gl.uniform1f(loc, 0)));
+    shader.add_uniform("uFogFar", new Uniform(loc => gl.uniform1f(loc, 0)));
+  }
+}
--- src/shaders/fragment_shader.js
+++ src/shaders/fragment_shader.js
@@ -1,80 +1,5 @@
-function vertex(lut_size) { 
-return (
+export const fragment_shader_src =
 `#version 300 es
-
-precision mediump float;
-precision mediump sampler3D;
-precision mediump sampler2D;
-precision mediump int;
-
-in vec3 position;
-in vec3 normal;
-
-
-uniform mat4 uModel;
-uniform mat4 uView;
-uniform mat4 uProjection;
-uniform vec3 uGridSize;
-uniform int uScalingEnabled;
-
-uniform sampler2D uStateColourTexture;
-uniform sampler3D uStateTexture;
-
-vec3 centre = vec3(0.5, 0.5, 0.5);
-
-out vec4 vColour;
-out vec3 vNormal;
-out vec3 vFragPos;
-
-vec3 calculate_position(float index) {
-    float remain = float(gl_InstanceID);
-    float z = floor(remain/(uGridSize.x*uGridSize.y));
-    remain = remain - z*uGridSize.x*uGridSize.y;
-    float y = floor(remain/uGridSize.x);
-    float x = remain-y*uGridSize.x; 
-    return vec3(x, y, z);
-}
-
-void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
-
-    // vec3 colour = normalize(new_position / (uGridSize * 2.0));
-    // vColour = vec4(colour, 1);
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[0];
-
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-
-    vec3 distance = new_position-uGridSize/2.0;
-    float normalised_distance = length(distance/ (uGridSize/2.0));
-    normalised_distance = clamp(normalised_distance, 0.0, 1.0);
-    vec4 distance_colour = texture(uStateColourTexture, vec2(normalised_distance, 0));
-
-
-    vColour = state_colour; 
-    // vColour = vec4(distance_colour.xyz, state_colour.a); 
-
-
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
-}
-`
-)};
-
-function frag(total_lights) {
-return (
-`#version 300 es
-
-#define TOTAL_LIGHTS ${total_lights}
 
 precision mediump float;
 precision mediump int;
@@ -185,7 +110,4 @@ void main() {
     result = apply_fog(result, distance);
 
     fragColour = result;
-}`
-)};
-
-export default {vertex: vertex, frag: frag};
+}`;
