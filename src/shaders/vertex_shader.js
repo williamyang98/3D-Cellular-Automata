b2a7f75b2277e2e64bbe1be1af743dd2e2c85c1a
FiendChain <formulating.response@gmail.com>
2020-05-27 23:48:34 +1000

    Refactored vertex shaders for point clouds

--- src/shaders/vertex_shader.js
+++ src/shaders/vertex_shader.js
@@ -1,53 +1,15 @@
-function create_vertex_shader(main, body='') {
-return (
-`#version 300 es
-
-precision mediump float;
-precision mediump sampler3D;
-precision mediump sampler2D;
-precision mediump int;
-
-in vec3 position;
-in vec3 normal;
-
-
-uniform mat4 uModel;
-uniform mat4 uView;
-uniform mat4 uProjection;
-uniform vec3 uGridSize;
-
-uniform int uScalingEnabled;
-
-uniform sampler2D uStateColourTexture;
-uniform sampler2D uRadiusColourTexture;
-uniform sampler3D uStateTexture;
-
-uniform vec3 uViewPosition;
-
-vec3 centre = vec3(0.5, 0.5, 0.5);
-
-out vec4 vColour;
-out vec3 vNormal;
-out vec3 vFragPos;
-
-vec3 calculate_position(float index) {
-    float remain = float(gl_InstanceID);
+const calculate_offset = (
+`vec3 calculate_offset(int index) {
+    float remain = float(index);
     float z = floor(remain/(uGridSize.x*uGridSize.y));
     remain = remain - z*uGridSize.x*uGridSize.y;
     float y = floor(remain/uGridSize.x);
     float x = remain-y*uGridSize.x; 
     return vec3(x, y, z);
-}
+}`);
 
-${body}
-
-${main}
-`
-)};
-
-const state_shading = create_vertex_shader(
-`
-mat3 Rx(float a) {
+const calculate_point_cloud = (
+`mat3 Rx(float a) {
     return mat3(
         1., 0., 0.,
         0., cos(a), sin(a),
@@ -63,188 +25,184 @@ mat3 Ry(float a) {
     );
 }
 
-vec3 calculate_point_cloud(vec3 pos, vec3 offset) {
-    vec3 center = vec3(0.5, 0.5, 0.5);
-    vec3 view_direction = normalize(uViewPosition-offset+(uGridSize/2.0)-center);
+vec3 calculate_point_cloud(const vec3 pos, const vec3 offset) {
+    vec3 point_position = offset+uCenter-(uGridSize/2.0);
+    vec3 view_direction = uViewPosition-point_position;
+    vec2 xz = vec2(view_direction.x, view_direction.z);
+    float r = length(xz);
     float ay = atan(view_direction.x, view_direction.z);
-    float r = pow(pow(view_direction.x, 2.0) + pow(view_direction.z, 2.0), 0.5);
     float ax = -atan(view_direction.y, r);
     mat3 R = Ry(ay) * Rx(ax);
-    return R*(position-center) + center; 
+    return R*(pos-uCenter) + uCenter + offset; 
+}`
+);
+
+const get_cell_data = (
+`
+vec4 get_cell_data(vec3 offset) {
+    vec3 vol_tex_coords = offset / uGridSize;
+    vec4 cell = texture(uStateTexture, vol_tex_coords);
+    return cell;
 }
-
-
-void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
-    vec3 vPosition = calculate_point_cloud(position, offset);
-
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[0];
-
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-vPosition;
-    vec3 new_position = vPosition + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-    vColour = state_colour; 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
-}`);
-
-const xyz_shading = create_vertex_shader(
-`void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
-
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[0];
-
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-    vec3 cube_colour = normalize(offset / uGridSize);
-
-    vColour = vec4(cube_colour.xyz, state_colour.a); 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
-}`);
-
-const layer_shading = create_vertex_shader(
 `
+);
+
+const calculate_scaling = (
+`
+vec3 scale_position(float scale, vec3 pos) {
+    float K = max(scale, float(1-uScalingEnabled));
+    vec3 delta = pos-uCenter;
+    return uCenter + (K*delta);
+}
+`
+);
+
+const inline_imports = (
+`
+${calculate_offset}
+${calculate_scaling}
+${calculate_point_cloud}
+${get_cell_data}
+`
+);
+
+const create_inline_snippet = (point_cloud) => (
+`
+// get basic info about cell
+vec3 offset = calculate_offset(gl_InstanceID);
+vec4 cell = get_cell_data(offset);
+float state = cell[0];
+float neighbours = cell[1];
+vec4 state_colour =  texture(uStateColourTexture, vec2(state,0));
+`
+);
+
+const create_inline_header = (point_cloud) => (
+`#version 300 es
+
+precision mediump float;
+precision mediump sampler3D;
+precision mediump sampler2D;
+precision mediump int;
+
+in vec3 position;
+${point_cloud ? '' : 'in vec3 normal;'}
+
+// MVP
+uniform mat4 uModel;
+uniform mat4 uView;
+uniform mat4 uProjection;
+uniform vec3 uViewPosition;
+uniform vec3 uGridSize;
+// params
+uniform int uScalingEnabled;
+// texturing
+uniform sampler2D uStateColourTexture;
+uniform sampler2D uRadiusColourTexture;
+uniform sampler3D uStateTexture;
+
+vec3 uCenter = vec3(0.5, 0.5, 0.5);
+
+out vec4 vColour;
+${point_cloud ? '' : 'out vec3 vNormal;'}
+${point_cloud ? '' : 'out vec3 vFragPos;'}
+
+${inline_imports}
+`
+)
+
+const create_inline_footer = (point_cloud) => (
+`
+${point_cloud ? 
+    'vPosition = calculate_point_cloud(vPosition, offset);' : 
+    'vPosition = vPosition + offset;'}
+
+// pass through data
+vPosition *= vColour.a;
+mat4 MVP = uProjection * uView * uModel;
+${point_cloud ? '' : 'vNormal = normal;'}
+${point_cloud ? '' : 'vFragPos = vec3(uModel * vec4(vPosition, 1.0));'}
+gl_Position = MVP * vec4(vPosition, 1.0);
+`
+);
+
+const create_state_shader = (point_cloud) => (
+`${create_inline_header(point_cloud)}
 void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
-
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[0];
-
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-
-    vec3 distance = offset-uGridSize/2.0;
-    // repeat every n blocks
-    float normalised_distance = length(distance/10.0);
-    normalised_distance = mod(normalised_distance, 1.0);
-
-    // scale to size of grid and repeat n times
-    // float normalised_distance = length(distance/ (uGridSize/2.0));
-    // float total_repeats = 1.0;
-    // normalised_distance = clamp(normalised_distance, 0.0, 1.0) * total_repeats;
-
-    vec4 distance_colour = texture(uRadiusColourTexture, vec2(normalised_distance, 0));
-
-    vColour = vec4(distance_colour.xyz, state_colour.a); 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
+    ${create_inline_snippet(point_cloud)}
+    vec3 vPosition = scale_position(state, position);
+    vColour = state_colour;
+    ${create_inline_footer(point_cloud)}
 }`);
 
-const radius_shading = create_vertex_shader(
-`
+const create_xyz_shading = (point_cloud) => (
+`${create_inline_header(point_cloud)}
 void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
+    ${create_inline_snippet(point_cloud)}
+    vec3 vPosition = scale_position(state, position);
+    vec3 xyz_colour = offset / uGridSize;
+    vColour = vec4(xyz_colour, state_colour.a);
+    ${create_inline_footer(point_cloud)}
+}
+`);
 
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[0];
+const create_layer_shading = (point_cloud) => (
+`${create_inline_header(point_cloud)}
+void main() {
+    ${create_inline_snippet(point_cloud)}
+    vec3 vPosition = scale_position(state, position);
+    vec3 distance = offset - (uGridSize/2.0);
+    float dist = length(distance/10.0);
+    dist = mod(dist, 1.0);
+    vec4 dist_colour = texture(uRadiusColourTexture, vec2(dist, 0.0));
+    vColour = vec4(dist_colour.xyz, state_colour.a);
+    ${create_inline_footer(point_cloud)}
+}`);
 
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-
-    vec3 distance = offset-uGridSize/2.0;
-    // repeat every n blocks
-    // float normalised_distance = length(distance/10.0);
-    // normalised_distance = mod(normalised_distance, 1.0);
-
-    // scale to size of grid and repeat n times
-    // float normalised_distance = length(distance)/length(uGridSize/2.0);
-    float normalised_distance = length(distance/(uGridSize/2.0));
+const create_radius_shading = (point_cloud) => (
+`${create_inline_header(point_cloud)}
+void main() {
+    ${create_inline_snippet(point_cloud)}
+    vec3 xyz_center = uGridSize/2.0;
+    vec3 distance = offset-xyz_center;
+    float radius = length(distance/xyz_center);
     float total_repeats = 1.0;
-    normalised_distance = clamp(normalised_distance, 0.0, 1.0) * total_repeats;
+    radius = clamp(radius, 0.0, 1.0) * total_repeats;
+    vec4 radius_colour = texture(uRadiusColourTexture, vec2(radius, 0.0));
 
-    vec4 distance_colour = texture(uRadiusColourTexture, vec2(normalised_distance, 0));
-
-    vColour = vec4(distance_colour.xyz, state_colour.a); 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
+    vec3 vPosition = scale_position(state, position);
+    vColour = vec4(radius_colour.xyz, state_colour.a);
+    ${create_inline_footer(point_cloud)}
 }`);
 
-const neighbour_shading = create_vertex_shader(
-`void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
+const create_neighbour_shading = (point_cloud) => (
+`${create_inline_header(point_cloud)}
+void main() {
+    ${create_inline_snippet(point_cloud)}
+    vec4 neighbour_colour = texture(uStateColourTexture, vec2(neighbours, 0.0));
 
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float index = result[1];
-
-    float scale = max(index, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(index,0));
-    vColour = state_colour; 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
+    vec3 vPosition = scale_position(neighbours, position);
+    vColour = neighbour_colour;
+    ${create_inline_footer(point_cloud)}
 }`);
 
-const neighbour_and_alive_shading = create_vertex_shader(
-`void main() {
-    vec3 offset = calculate_position(float(gl_InstanceID));
+const create_neighbour_and_alive_shading = (point_cloud) => (
+`${create_inline_header(point_cloud)}
+void main() {
+    ${create_inline_snippet(point_cloud)}
+    vec4 neighbour_colour = texture(uStateColourTexture, vec2(neighbours, 0.0));
 
-    vec3 state_lookup = offset / uGridSize;
-    vec4 result = texture(uStateTexture, state_lookup);
-    float state = result[0];
-    float neighbours = result[1];
-
-    float scale = max(neighbours, float(1-uScalingEnabled));
-    vec3 to_centre = centre-position;
-    vec3 new_position = position + to_centre*(1.0-scale) + offset;
-
-    vec4 state_colour =  texture(uStateColourTexture, vec2(state,0));
-    vec4 neighbour_colour = texture(uStateColourTexture, vec2(neighbours, 0)); 
-    
-    vColour = vec4(neighbour_colour.xyz, state_colour.a*neighbour_colour.a); 
-    new_position *= vColour.a;
-    vNormal = normal;
-    vFragPos = vec3(uModel * vec4(new_position, 1));
-
-    mat4 MVP = uProjection * uView * uModel;
-    gl_Position = MVP * vec4(new_position, 1);
+    vec3 vPosition = scale_position(neighbours, position);
+    vColour = vec4(neighbour_colour.xyz, state_colour.a*neighbour_colour.a);
+    ${create_inline_footer(point_cloud)}
 }`);
+
 
 export const vertex_shader_src = {
-    state: state_shading,
-    xyz: xyz_shading,
-    layer: layer_shading,
-    radius: radius_shading,
-    neighbour: neighbour_shading,
-    'neighbour and alive': neighbour_and_alive_shading,
+    state: create_state_shader,
+    xyz: create_xyz_shading,
+    layer: create_layer_shading,
+    radius: create_radius_shading,
+    neighbour: create_neighbour_shading,
+    neighbour_and_alive: create_neighbour_and_alive_shading,
 };
