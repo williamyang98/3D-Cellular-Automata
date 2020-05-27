e223d6579c8e8afda423361716a3b8c844f54590
FiendChain <formulating.response@gmail.com>
2020-05-27 23:49:35 +1000

    Added additional debugging info for failed shader compilation

--- src/gl/Shader.js
+++ src/gl/Shader.js
@@ -7,36 +7,7 @@ export class Shader {
   }
 
   create_shader_program(vertex_shader_src, fragment_shader_src) {
-    let gl = this.gl;
-
-    const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertex_shader, vertex_shader_src);
-    gl.compileShader(vertex_shader);
-    if (!gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS)) {
-      console.error(gl.getShaderInfoLog(vertex_shader));
-      throw new Error('Unable to compile vertex shader');
-    }
-
-    const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragment_shader, fragment_shader_src);
-    gl.compileShader(fragment_shader);
-    if (!gl.getShaderParameter(fragment_shader, gl.COMPILE_STATUS)) {
-      console.error(gl.getShaderInfoLog(fragment_shader));
-      throw new Error('Unable to compile fragment shader');
-    }
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vertex_shader);
-    gl.attachShader(program, fragment_shader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      console.error(gl.getProgramInfoLog(program));
-      throw new Error('Unable to construct shader program');
-    }
-
-    this.vertex_shader = vertex_shader;
-    this.fragment_shader = fragment_shader;
-    this.program = program;
+    [this.vertex_shader, this.fragment_shader, this.program] = create_program(this.gl, vertex_shader_src, fragment_shader_src); 
   }
 
   // add to list of uniforms permanently binded to shader
@@ -71,4 +42,41 @@ export class Shader {
       uniform.apply(location);
     }
   }
+}
+
+function create_program(gl, vertex_shader_src, fragment_shader_src) {
+  const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertex_shader, vertex_shader_src);
+  gl.compileShader(vertex_shader);
+  if (!gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(vertex_shader));
+    console.error(prepend_line_numbers(vertex_shader_src));
+    throw new Error('Unable to compile vertex shader');
+  }
+
+  const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragment_shader, fragment_shader_src);
+  gl.compileShader(fragment_shader);
+  if (!gl.getShaderParameter(fragment_shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(fragment_shader));
+    throw new Error('Unable to compile fragment shader');
+  }
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertex_shader);
+  gl.attachShader(program, fragment_shader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+    console.error(prepend_line_numbers(fragment_shader_src));
+    throw new Error('Unable to construct shader program');
+  }
+
+  return [vertex_shader, fragment_shader, program];
+}
+
+function prepend_line_numbers(src) {
+  let lines = src.split('\n').map((v, i) => `${i+1}\t| ${v}`);
+  let out = lines.join('\n');
+  return out;
 }
