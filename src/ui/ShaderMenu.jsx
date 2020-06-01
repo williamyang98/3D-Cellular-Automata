b5f644e3ec8fd886ced09a02952edb199177c432
FiendChain <formulating.response@gmail.com>
2020-06-01 18:13:37 +1000

    Updated shader menu with refactored renderers

--- src/ui/ShaderMenu.jsx
+++ src/ui/ShaderMenu.jsx
@@ -4,68 +4,17 @@ import { RenderAdjustableValue } from './AdjustableValueViews';
 
 export function ShaderMenu() {
   const dispatch = useDispatch();
-
-  const render_types = useSelector(state => state.shader_manager.render_types);
-  const current_render_type = useSelector(state => state.shader_manager.current_render_type);
-  const colourings = useSelector(state => state.shader_manager.colourings);
-  const current_colouring = useSelector(state => state.shader_manager.current_colouring);
-  const shadings = useSelector(state => state.shader_manager.shadings);
-  const current_shading = useSelector(state => state.shader_manager.current_shading);
-
-  function select_render_type(event) {
-    let index = event.target.value;
-    dispatch({type:'shader.select_render_type', value:index});
-  }
-
-  function select_colouring(event) {
-    let index = event.target.value;
-    dispatch({type:'shader.select_colouring', value:index});
-  }
-
-  function select_shading(event) {
-    let index = event.target.value;
-    dispatch({type:'shader.select_shading', value:index});
-  }
-
-  const render_type_options = render_types.map((render_type, i) => {
-    return <option value={i} key={`render_type_${i}`}>{render_type.name}</option>
-  })
-
-  const colouring_options = colourings.map((name, i) => {
-    return <option value={i} key={`colouring_${i}`}>{name}</option>
-  })
-
-  const shading_options = shadings.map((name, i) => {
-    return <option value={i} key={`shading_${i}`}>{name}</option>
-  })
+  const renderer_type = useSelector(state => state.shader_manager.renderer_type);
+  const current_renderer_type = useSelector(state => state.shader_manager.renderer_type.value);
 
   return (
     <div className="card">
       <div className='card-header'>Shaders</div>
       <div className="card-body">
         <form className='form-inline'>
-          <div className='form-group'>
-            <label className='mr-2'>Colouring </label>
-            <select className='custom-select custom-select-sm' value={current_colouring} onChange={select_colouring}>
-              {colouring_options}
-            </select>
-          </div>
-        </form>
-        <form className='form-inline'>
-          <div className='form-group'>
-            <label className='mr-2'>Shading </label>
-            <select className='custom-select custom-select-sm' value={current_shading} onChange={select_shading}>
-              {shading_options}
-            </select>
-          </div>
-        </form>
-        <form className='form-inline'>
-          <div className='form-group'>
-            <label className='mr-2'>Render Type </label>
-            <select className='custom-select custom-select-sm' value={current_render_type} onChange={select_render_type}>
-              {render_type_options}
-            </select>
-          </div>
+          {RenderAdjustableValue(renderer_type, 0, 'Renderer', value => {
+            dispatch({type:'shader.select_renderer', value:value});
+          })}
         </form>
         <hr></hr>
         <ShaderSettings></ShaderSettings>
@@ -76,22 +25,15 @@ export function ShaderMenu() {
 
 function ShaderSettings() {
   const dispatch = useDispatch();
-  const shader_params = useSelector(state => state.shader_manager.params);
-
-  function set_param(name, value) {
-    let action = {type:'shader.set_param', name:name, value:value};
-    dispatch(action);
-  }
-
-  const params = Object
-    .entries(shader_params)
-    .map(([name, param], index) => {
-      return RenderAdjustableValue(param, index, name, value => {
-        set_param(name, value);
-      })
-    });
+  const params = useSelector(state => state.shader_manager.params);
 
   return (
-    <form>{params}</form>
+    <form>{Object.entries(params).map(([name, param], index) => {
+      return RenderAdjustableValue(param, index, name, value => {
+        let data = {};
+        data[name] = value;
+        dispatch({type: 'shader.update_params', value: data}) 
+      })
+    })}</form>
   );
 }
