35a639a257e76668d3596409b143a2587f1d98a9
FiendChain <formulating.response@gmail.com>
2020-06-12 00:13:50 +1000

    Added code for service worker from react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <App></App>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
+// serviceWorker.register();
