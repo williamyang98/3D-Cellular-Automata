4bcf621900248c5389391d655e0406afcb31d711
FiendChain <formulating.response@gmail.com>
2020-07-09 02:26:34 +1000

    Add github link

--- src/ui/components/SimulationView/SimulationView.jsx
+++ src/ui/components/SimulationView/SimulationView.jsx
@@ -31,11 +31,27 @@ export function SimulationView(props) {
     );
   }
 
+  function render_links() {
+    return (
+      <div className={float_controls} style={{zIndex:3, position:'absolute', top:'1.5rem', left:'1.5rem'}}>
+        <a className="btn btn-dark btn-circle btn-md" 
+          href="https://github.com/FiendChain/3D-Cellular-Automata"
+          target="_blank"
+          data-toggle="tooltip"
+          data-placement="left"
+          title="Github Repository">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+    )
+  }
+
   return (
     <div className="card shadow h-100">
       <Canvas store={store} canvas={props.canvas}></Canvas>
       {app ? render_float_controls() : <div></div>}
       {render_fullscreen_button()}
+      {render_links()}
     </div>
   );
 }
